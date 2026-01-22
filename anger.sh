1cc9c30a5b41bced08595edd5ba3bd62ae9c5610
maunzCache <166729+maunzCache@users.noreply.github.com>
2026-01-22 20:45:00 +0100

    Add function to break your system if using apt

--- anger.sh
+++ anger.sh
@@ -8,3 +8,8 @@ alias tableflip="goddamn shutdown now"
 
 # https://github.com/nvbn/thefuck
 alias get-fucked="pip install thefuck"
+
+function breaksystem {
+  # Replace with your system update command
+  goddamn apt update && goddamn apt upgrade - y
+}
